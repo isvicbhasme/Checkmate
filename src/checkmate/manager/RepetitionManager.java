d997fcd279349190b5dbbad770d18ad2203e730f
isvicbhasme <isvicbhasme@live.com>
2014-07-01 22:41:34 +0530

    Support of Zobrist Hashing for castling, enPassant, side to move

--- src/checkmate/manager/RepetitionManager.java
+++ src/checkmate/manager/RepetitionManager.java
@@ -11,23 +11,26 @@ import checkmate.util.PieceInfo;
 import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
-import jdk.nashorn.internal.codegen.types.BitwiseType;
 
 /**
  *
- * @author basme
+ * @author bhasme
  */
 public class RepetitionManager {
 
     private final int numOfCells = CellInfo.Rank.size * CellInfo.File.size;
     private final int numOfPieces = PieceInfo.Type.size;
     private final long[][] bitStringTable = new long[numOfPieces][numOfCells];
+    private long whitesTurnToPlay;
+    private long[] castlingRights = new long[4];
+    private long[] enPassantAvailables = new long[CellInfo.File.size];
     private Queue hashHistory = new ArrayBlockingQueue(100);
     private long zobristKey = 0;
     private int seed = 0;
 
     private RepetitionManager() {
         initBitStringTable();
+        initZobristKey();
     }
 
     public static RepetitionManager getInstance() {
@@ -45,25 +48,52 @@ public class RepetitionManager {
             for (int j = 0; j < numOfCells; j++) {
                 random.setSeed(seed++);
                 bitStringTable[i][j] = random.nextLong();
-                if(bitStringTable[i][j] < 0)
-                    bitStringTable[i][j] *= -1;
             }
+        }
+        random.setSeed(seed++);
+        whitesTurnToPlay = random.nextLong();
+        for (int i=0; i< castlingRights.length; i++) {
+            random.setSeed(seed++);
+            castlingRights[i] = random.nextLong();
+        }
+        for (int i=0; i< enPassantAvailables.length; i++) {
+            random.setSeed(seed++);
+            enPassantAvailables[i] = random.nextLong();
         }
     }
     
+    private void initZobristKey() {
+        zobristKey ^= whitesTurnToPlay;
+        for (int i=0; i< castlingRights.length; i++) {
+            zobristKey ^= castlingRights[i];
+        }
+    }
+
     public boolean isCurrentMoveRepeated() {
         return hashHistory.contains(zobristKey);
     }
-    
+
     public void storeHash() {
         hashHistory.add(zobristKey);
     }
 
     public void hash(PieceInfo.Type pieceType, CellInfo.Rank rank, CellInfo.File file) {
-        zobristKey = zobristKey ^ bitStringTable[pieceType.ordinal()][(rank.ordinal()*8) + file.ordinal()];
+        zobristKey = zobristKey ^ bitStringTable[pieceType.ordinal()][(rank.ordinal() * 8) + file.ordinal()];
     }
 
     public void hash(PieceInfo.Type pieceType, Address address) {
         hash(pieceType, address.rank, address.file);
+    }
+    
+    public void hashTogglePlay() {
+        zobristKey ^= whitesTurnToPlay;
+    }
+    
+    public void hashEnPassant(CellInfo.File enPassantFile) {
+        zobristKey ^= enPassantAvailables[enPassantFile.ordinal()];
+    }
+    
+    public void hashCastlingRights(int position) {
+        zobristKey ^= castlingRights[position];
     }
 }
